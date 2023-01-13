d1fa48c053fd71016ee423a0ef04f5744f0c92ec
Hongnick <nick55002@gmail.com>
2023-01-13 15:21:20 +0800

    刪除功能

--- LightdowBuy/LightdowBuy/Protocol/ShoppingcartTableViewCellDelegate.swift
+++
@@ -1,7 +0,0 @@
-
-
-import Foundation
-protocol ShoppingcartTableViewCellDelegate {
-    func showMessage(with message:String)
-    func confirmAction(with completionHandler: @escaping (Bool?) -> Void)
-}
